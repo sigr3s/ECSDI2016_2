a0340d22f9a26c796074b8e2623b1275fa8526b7
Aleix Pellisa Cortiella <aleix.pellisa@a5s113pc25.fib.upc.es>
2016-05-12 08:44:47 +0200

    first commit

--- ecsdiLAB/practica.py
+++ ecsdiLAB/practica.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return json.dumps({"asdFasd": [{"a":"b"},{"c": "d"}]})
 
 
 if __name__ == '__main__':
