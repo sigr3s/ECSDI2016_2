519703b3e551f14d54b5ffd716545ec72615c595
Sigr3s <tortosa95@gmail.com>
2016-05-22 13:44:50 +0200

    rdf hates

--- ecsdiLAB/ProductService.py
+++ ecsdiLAB/ProductService.py
@@ -1,4 +1,6 @@
 from rdflib import Graph
+from rdflib.namespace import RDF, RDFS, Namespace, FOAF, OWL
+
 
 
 class ProductService:
@@ -7,4 +9,19 @@ class ProductService:
         self.products = Graph().parse("products.rdf", format="turtle")
 
     @classmethod
+    def initialize(cls):
+        PrE = Namespace("http://www.products.org/ontology/")
+        PrEP = Namespace("http://www.products.org/ontology/property/")
+        PrER = Namespace("http://www.products.org/ontology/resource/")
+        cls.products.bind('prod', PrE)
+        cls.products.bind('prodprop', PrEP)
+        cls.products.bind('prodres', PrER)
+        cls.products.add((PrE.InternalProduct, RDF.type, OWL.Class))
+
+
+    @classmethod
     def search(self, name, price_min, price_max, seller, weight_min, weight_max):
+        qres = self.products.query(
+
+        )
+        return
