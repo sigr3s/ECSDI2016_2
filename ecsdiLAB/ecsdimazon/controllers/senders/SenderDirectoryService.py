91b29a8f3974a180bf2be2962a60ec943133f0eb
oriol.munoz <Hanekoma@users.noreply.github.com>
2016-06-05 01:54:52 +0200

    fix sender directory

--- ecsdiLAB/ecsdimazon/controllers/senders/SenderDirectoryService.py
+++ ecsdiLAB/ecsdimazon/controllers/senders/SenderDirectoryService.py
@@ -14,7 +14,7 @@ class SenderDirectoryService:
     def __init__(self):
         import os
         if not os.path.exists(SENDERS_FILE_NAME):
-            open('catalog.rdf', 'w')
+            open(SENDERS_FILE_NAME, 'w')
         self.senders = Graph().parse(SENDERS_FILE_NAME, format='turtle')
 
     def senders_list(self, graph):  # graph ignored on purpose, not needed
