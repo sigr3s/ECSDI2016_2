824f84cf82d8d6a0aa0d516504fdb35660f4f2fd
aleixpellisa <aleix.pellisa@est.fib.upc.edu>
2016-06-04 14:12:30 +0200

    modificat la interficie del updater, afegir el update message i modificat el Aupdater per a que, entre tots, pugin productes a catalog.rdf

--- ecsdiLAB/ecsdimazon/controllers/AUpdater.py
+++ ecsdiLAB/ecsdimazon/controllers/AUpdater.py
@@ -1,21 +1,60 @@
 import json
+import sys
 
-from rdflib import Graph, Literal
 from flask import Flask, request
+from rdflib import Graph
 
 from ecsdiLAB.ecsdimazon.context.ECSDIContext import ECSDIContext
+from ecsdiLAB.ecsdimazon.controllers import AgentUtil
 from ecsdiLAB.ecsdimazon.controllers import Constants
+from ecsdiLAB.ecsdimazon.messages import Ontologies
+from ecsdiLAB.ecsdimazon.messages.UploadProductMessage import UploadProductMessage
 from ecsdiLAB.ecsdimazon.model.Product import Product
 
 app = Flask(__name__)
 
 context = ECSDIContext()
 
-@app.route('/catalog', methods=['POST'])
-def new_product_in_catalog():
-    product = Product.from_graph(Graph().parse(data=request.get_data(as_text=True), format='xml'))
-    context.product_service.upload_in_catalog(product)
-    return json.dumps(product)
+
+@app.route('/')
+def hello_world():
+    links = []
+    for rule in app.url_map.iter_rules():
+        methods = str(rule.methods)
+        route = rule.rule
+        links.append(methods + " : " + route)
+    return json.dumps(links)
+
+
+def upload_product(graph):
+    upm = UploadProductMessage.from_graph(graph)
+    upload_product_ean = upm.ean
+    upload_product_name = upm.name
+    upload_product_brand = upm.brand
+    upload_product_price = upm.price
+    upload_product_height = upm.height
+    upload_product_widht = upm.width
+    upload_product_weight = upm.weight
+
+    product = Product(upload_product_ean, upload_product_name, upload_product_brand,
+                      upload_product_price, upload_product_height, upload_product_widht,
+                      upload_product_weight, upload_product_name)
+
+    uploaded_product = context.product_service.upload_in_catalog(product)
+
+    return Product.list_to_graph(uploaded_product).serialize()
+
+
+@app.route('/comm', methods=['GET', 'POST'])
+def comm():
+    graph = Graph().parse(data=request.data, format='xml')
+    ontology = AgentUtil.ontology_of_message(graph)
+    return routings[ontology](graph)
+
+
+routings = {
+    Ontologies.UPLOAD_PRODUCT_MESSAGE: upload_product
+}
 
 if __name__ == '__main__':
-    app.run(port=Constants.PORT_AUPDATER,debug=True)
+    app.run(port=Constants.PORT_AUPDATER, debug=True)
---
+++ ecsdiLAB/ecsdimazon/messages/UploadProductMessage.py
@@ -0,0 +1,59 @@
+from rdflib import Graph, Literal
+from rdflib.namespace import Namespace, FOAF
+
+from ecsdiLAB.ecsdimazon.controllers import Constants
+
+
+class UploadProductMessage:
+    def __init__(self, ean, name, brand, price, height, width, weight):
+        self.ean = ean
+        self.name = name
+        self.brand = brand
+        self.price = price
+        self.height = height
+        self.width = width
+        self.weight = weight
+
+    def to_graph(self):
+        graph = Graph()
+        n = Namespace(Constants.NAMESPACE)
+        p = n.__getattr__('#Product#' + str(self.ean))
+        graph.add((p, FOAF.EAN, Literal(self.ean)))
+        graph.add((p, FOAF.Name, Literal(self.name)))
+        graph.add((p, FOAF.Brand, n.__getattr__('#Brand#' + str(self.brand))))
+        graph.add((p, FOAF.Price, Literal(self.price)))
+        graph.add((p, FOAF.Height, Literal(self.height)))
+        graph.add((p, FOAF.Width, Literal(self.width)))
+        graph.add((p, FOAF.Weight, Literal(self.weight)))
+        return graph
+
+    @classmethod
+    def list_to_graph(cls, upms):
+        graph = Graph()
+        for upm in upms:
+            graph = graph + upm.to_graph()
+        return graph
+
+    @classmethod
+    def from_graph(cls, graph):
+        query = """SELECT ?x ?ean ?name ?brand ?price
+                            ?height ?width ?weight
+            WHERE {
+                ?x ns1:EAN ?ean.
+                ?x ns1:Name ?name.
+                ?x ns1:Brand ?brand.
+                ?x ns1:Price ?price.
+                ?x ns1:Height ?height.
+                ?x ns1:Width ?width.
+                ?x ns1:Weight ?weight.
+            }"""
+        qres = graph.query(query)
+        for p, ean, name, brand, price, height, width, weight in qres:
+            return UploadProductMessage(
+                ean.toPython(),
+                name.toPython(),
+                brand.toPython(),
+                price.toPython(),
+                height.toPython(),
+                width.toPython(),
+                weight.toPython(),)
